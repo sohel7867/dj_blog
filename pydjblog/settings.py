f933be4ef5bf56c59eee4d1f5f8e0af1b40a4c52
sohel <sohelmira28@gmail.com>
2020-08-09 12:44:14 +0530

    import django_heroku added

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,5 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sohelmira28@gmail.com'
 EMAIL_HOST_PASSWORD = 'Shivani.mane@gmail.com'
+
+django_heroku.settings(locals())  
